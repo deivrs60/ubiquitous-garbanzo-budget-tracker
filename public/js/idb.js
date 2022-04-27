5322ffa471256a50f978d7cf9b40bdca77da2220
David Reyes Santoyo <deiv@Deivs-MBP.local>
2022-04-27 11:32:07 -0700

    transactions now post to the host when internet connectivity is regained. When internet connectivity is off it stores the data in indexedDB

--- public/js/idb.js
+++ public/js/idb.js
@@ -21,7 +21,7 @@ request.onsuccess = function(event) {
     // check if app is online, if yes run uploadTransaction() functino to send all local db data to api
     if (navigator.onLine) {
         // need to create first 
-        // uploadTransaction();
+         uploadTransaction();
     }
 };
 
@@ -29,3 +29,61 @@ request.onerror  = function(event) {
     // log error here
     console.log(event.target.errorCode);
 };
+
+// This function will execute if we attempt to submit a new transaction and there's no internet connection
+function saveRecord(record) {
+    // open a new transaction with the database with read and write permissions
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
+
+    // access the object store for 'new_transaction'
+    const budgetObjectStore = transaction.objectStore('new_transaction');
+
+    // add record to your store with add method
+    budgetObjectStore.add(record);
+}
+
+function uploadTransaction() {
+    // open a transaction on your db
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
+
+    // access your object store
+    const budgetObjectStore = transaction.objectStore('new_transaction');
+
+    // get all records from store and set to a variable
+    const getAll = budgetObjectStore.getAll();
+
+    // upon a successful .getAll() execution, this function runs
+    getAll.onsuccess = function() {
+        // if there was data in indexedDb's store, let's send it to the api server
+        if (getAll.result.length > 0)  {
+            fetch('/api/transaction', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json'
+                }
+            })
+            .then(response => response.json())
+            .then(serverResponse => {
+                if(serverResponse.message) {
+                    throw new Error(serverResponse);
+                }
+                // open one more transaction 
+                const transaction = db.transaction(['new_transaction'], 'readwrite');
+                // acess the new_transaction object store
+                const budgetObjectStore = transaction.objectStore('new_transaction');
+                budgetObjectStore.clear();
+
+                alert('All saved transactions have been submitted!');
+
+            })
+            .catch(err => {
+                console.log(err);
+            });
+        }
+    };
+}
+
+// listen for app coming back online
+window.addEventListener('online', uploadTransaction);
